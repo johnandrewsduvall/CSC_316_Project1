4f1674b02125d6342c4ad3fafdeb348fcdd8419f
John Andrew S Duvall <johnandew.duvall@gmail.com>
2015-09-17 14:24:14 -0400

    First commit, initialization

---
+++ src/csc_316_project1/CSC_316_Project1.java
@@ -0,0 +1,17 @@
+/*
+ * Author:     John Andrew S Duvall, [put names here]
+ * Date:       27 Sep 2015
+ * Project:    CSC 316 Project1
+ */
+package csc_316_project1;
+
+public class CSC_316_Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
